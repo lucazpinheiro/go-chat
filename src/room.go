fac0b3586ade87110149290fb8da1c0bde0b26d4
Lucas <lucascpinheiro10@gmail.com>
2022-02-02 21:46:23 -0300

    feat: add types for client and room model

---
+++ src/room.go
@@ -0,0 +1,35 @@
+package main
+
+type room struct {
+	// forward is a channel that holds incoming messages
+	// that should be forwarded to the other clients.
+	forward chan []byte
+
+	// join is a channel for clients wishing to join the room.
+	join chan *Client
+
+	// leave is a channel for clients wishing to leave the room.
+	leave chan *Client
+
+	// clients holds all current clients in this room.
+	clients map[*Client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
+}
